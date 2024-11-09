1053908bc253615102bec1cf49611db81b9e8cc7
jorgeluisculisvillugas <culisvillugasjorgeluis@gmail.com>
2024-11-09 16:04:22 -0500

    agregado

--- src/main/java/com/controlarspam/ejercicio1/Ejercicio1Application.java
+++ src/main/java/com/controlarspam/ejercicio1/Ejercicio1Application.java
@@ -1,7 +1,13 @@
 package com.controlarspam.ejercicio1;
-
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.controlarspam.ejercicio1.Control_de_acceso.SpamFilterService;
+import com.controlarspam.ejercicio1.Interfaz_grafica.SpamFilterApp;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class Ejercicio1Application {
@@ -10,4 +16,12 @@ public class Ejercicio1Application {
 		SpringApplication.run(Ejercicio1Application.class, args);
 	}
 
+	@Bean
+CommandLineRunner init(SpamFilterService spamFilterService) {
+	return args -> SwingUtilities.invokeLater(() -> {
+		new SpamFilterApp(spamFilterService);
+	});
 }
+}
+
+
